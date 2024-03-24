2a850b1c03c254e81ba119cc0f8470c76341f35e
TAsaha <121726306+TAsaha@users.noreply.github.com>
2024-03-24 12:26:20 +0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("Text Generation")
 
 prompt = st.text_input("Enter a prompt to start:")
 temperature = st.slider("Temperature (Controls Randomness)", min_value=0.0, max_value=1.0, step=0.1, value=0.7)
-max_length = st.number_input("Max Length of Generated Text", min_value=10, max_value=200, step=10, value=100)
+max_length = st.number_input("Max Length of Generated Text", min_value=10, max_value=200, step=10, value=30)
 
 if st.button("Generate Text"):
   # Load the model if not cached
